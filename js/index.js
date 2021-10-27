56912b84533dd906046fc06ba9743e696d3e0723
Juan Pablo Herrera <jpherrera@mailoo.org>
2021-10-27 16:08:54 -0300

    js

--- js/index.js
+++ js/index.js
@@ -1,32 +1,21 @@
-let t = 0;
-const btn = document.getElementById('btn')
-btn.addEventListener("click", lafuncion);
-const tit = document.getElementById('tit');
-const inpu = document.getElementById('prueba')
-const img = document.getElementById('img')
+console.log("vinculado");
+const titulo = document.getElementById("titulo");
+const parrafo = document.getElementById("parrafo");
 
 
-
-console.log(inpu.getAttribute("type"))
-inpu.addEventListener("input" , cambiar)
-
-function cambiar() {
-    tit.innerHTML = inpu.value;
+if (titulo.classList.contains("rojo")) {
+    console.log("existe");
+    titulo.classList.remove("rojo");
+    titulo.classList.add("verde");
+}else{
+    console.log("no existe");
+    titulo.classList.add("rojo");
 }
 
-function lafuncion(){
-    t = t + 1;
-    console.log("hice clic "+img.getAttribute("src"));
-    if(tit.classList.contains("rojo")) {
-        tit.classList.remove("rojo");
-        tit.classList.add("verde");
-        tit.style.fontSize = "2em";
-        img.setAttribute("src","img/logo-trans-300.png");
-    }else{
-        tit.classList.remove("verde")
-        tit.classList.add("rojo")
-        tit.style.fontSize = "2.5em";
-        img.setAttribute("src","img/estudiantes.jpg");
-    }
-    tit.innerHTML = t;
+setTimeout(cambiarContenido, 5000);
+
+function cambiarContenido() {
+    parrafo.innerHTML="Mi perro es muy gruñon";
+
 }
+
