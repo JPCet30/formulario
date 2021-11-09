f6c5e43c07467f53273ddfa57455bc71301aff7e
Juan Pablo Herrera <jpherrera@mailoo.org>
2021-11-09 12:56:33 -0300

    color gradient

--- color/index.js
+++ color/index.js
@@ -12,27 +12,43 @@ let picker2 = document.getElementById("picker-2");
 let split = document.getElementById("split");
 let gradient = document.getElementById("gradient");
 
-picker1.oninput = updateGradient;
-picker2.oninput = updateGradient;
-split.oninput = updateGradient;
+split.addEventListener('input',algo);
+
+/*
+picker1.input = updateGradient;
+picker2.input = updateGradient;
+split.input = updateGradient;*/
 
 let color1 = "#b5caf9";
 let color2 = "#aad186";
 let splitPercent = 50;
-function updateGradient(e){
-    switch (e.target) {
-  case picker1:
-    color1 = e.target.value;
+//e es el evento y e.target se refiere elemento clickado
+function updateGradient(e) {
+  switch (e.target) {
+    case picker1:
+      color1 = e.target.value;
       break;
-  case picker2:
-    color2 = e.target.value;
+    case picker2:
+      color2 = e.target.value;
       break;
-  case split:
-    splitPercent = e.target.value;
+    case split:
+      splitPercent = e.target.value;
       break;
-    
-    }
-    gradient.style.background = `linear-gradient(to right, ${color1} , ${splitPercent}% , ${color2} )`;    
+
+  }
+
+
+/*
+
+  let gradiente = `linear-gradient(to right, ${color1} , ${splitPercent}% , ${color2} )`;
+  gradient.style.background = gradiente;
+  console.log(gradiente);*/
 }
 
+
+function algo() {
+  gradient.style.background = `linear-gradient(to right, ${picker1.value} ${split.value}%, ${picker2.value}`
+  //document.getElementById("codeblock").innerText = `linear-gradient(to right, ${colorOneEl.value} ${splitEl.value}%, ${colorTwoEl.value})`
+
+}
 
