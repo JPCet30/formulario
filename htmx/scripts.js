e3f207831544c92b5d21840b41a9f6beee11db49
JPCet30 <88854372+JPCet30@users.noreply.github.com>
2022-11-09 16:58:14 -0300

    Add files via upload

---
+++ htmx/scripts.js
@@ -0,0 +1,15 @@
+/* FUNCION closeModal para cerrar modal con htmx */ 
+
+function closeModal() {
+	var container = document.getElementById("modals-here")
+	var backdrop = document.getElementById("modal-backdrop")
+	var modal = document.getElementById("modal")
+
+	modal.classList.remove("show")
+	backdrop.classList.remove("show")
+
+	setTimeout(function() {
+		container.removeChild(backdrop)
+		container.removeChild(modal)
+	}, 200)
+}
